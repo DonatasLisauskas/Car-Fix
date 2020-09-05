821d4d57ba8ce88eb10cfae76b892cfd33703a88
donli <donliskas@gmail.com>
2020-09-05 18:22:49 +0300

    There's created class of Entity mapping "Customer" and added @ManyToOne and @OneToMany dependencies.

---
+++ src/main/java/entities/Customer.java
@@ -0,0 +1,29 @@
+package entities;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(schema = "carfixdatabase", name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customerId")
+    private Long customerId;
+
+    @Column(name = "firstName")
+    private String firstName;
+
+    @Column(name = "lastName")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "phoneNumber")
+    private Long phoneNumber;
+
+    @OneToMany(mappedBy = "customer")
+    private List<Registration> registrations;
+}

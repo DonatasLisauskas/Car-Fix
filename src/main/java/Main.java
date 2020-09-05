adfd04b12e285ba1a6016831e3388df94a8e5cab
donli <donliskas@gmail.com>
2020-09-05 17:16:55 +0300

    Modified Main.java file.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,29 +3,8 @@ import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 public class Main {
-
-    private static SessionFactory factory;
-
-    public static void setup() {
-    
-        
-        Configuration configuration = new Configuration();
-        configuration.configure("/hibernate.cfg.xml");
-                
-                
-        factory = configuration.buildSessionFactory();
-
-    }
-
     public static void main(String[] args) {
-
-        //Main.setup();
-
         HibernateUtil.getSessionFactory();
         HibernateUtil.shutdown();
-
-
-
     }
-
 }

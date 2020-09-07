e32338885c38603c48bdf930920d0407d7329cad
donli <donliskas@gmail.com>
2020-09-07 18:59:01 +0300

    Beta startup.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,14 @@
+import Visitor_Pattern.CarDaoPartDisplayVisitor;
+import Visitor_Pattern.DaoPart;
+import Visitor_Pattern.Visitors.CarDaoVisitor;
 import carfix.utils.HibernateUtil;
 
 public class Main {
     public static void main(String[] args) {
         HibernateUtil.getSessionFactory();
         HibernateUtil.shutdown();
+
+        DaoPart daoPart = new CarDaoVisitor().accept();
+
     }
 }

f1b20a7bb2860ed455724e6fea43543b64f98a5c
donli <donliskas@gmail.com>
2020-09-08 14:58:40 +0300

    JavaFX Demo v.1.0

---
+++ src/main/java/JavaFX/Buttons.java
@@ -0,0 +1,15 @@
+package JavaFX;
+
+import javafx.scene.control.Button;
+
+public interface Buttons {
+
+    Button startButton = new Button("Start");
+    Button nextButton = new Button("Next");
+    Button backButton = new Button("Back");
+    Button selectButton = new Button("Select");
+
+    Button carDaoButton = new Button("Car");
+    Button carServiceDaoButton = new Button("Car Service");
+
+}

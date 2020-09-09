4c2b204c9a6c306b2d9c2f3131a8abcd943932f6
donli <donliskas@gmail.com>
2020-09-09 19:47:13 +0300

    JavaFX/v1.3 Added fxml files and Controller.

---
+++ src/main/java/CarFixJavaFX/Controllers/CreateCarDBController.java
@@ -0,0 +1,31 @@
+package CarFixJavaFX.Controllers;
+
+import carfix.dao.CarDao;
+import carfix.entities.Car;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CreateCarDBController {
+
+    @FXML
+    private TextField seriesName;
+
+    @FXML
+    private TextField yearsOfManufacture;
+
+    @FXML
+    private TextField engineDisplacement;
+
+    @FXML
+    private TextField power;
+
+    @FXML
+    private Label label;
+
+    @FXML
+    public void selectButton() {
+        CarDao carDao = new CarDao();
+        carDao.createCar(new Car(seriesName.getText(), Long.valueOf(yearsOfManufacture.getText()), engineDisplacement.getText(), Long.valueOf(power.getText())));
+    }
+}

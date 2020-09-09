fe48de074751e8e3fdf7586b2dafe1332c2fe8ca
donli <donliskas@gmail.com>
2020-09-09 19:50:16 +0300

    JavaFX/v1.3 Added fxml files and Controller.

--- src/main/java/Visitor_Pattern/DaoPartVisitor.java
+++ src/main/java/Visitor_Pattern/DaoPartVisitor.java
@@ -1,24 +1,11 @@
 package Visitor_Pattern;
 
 import Facade_Pattern.DaoMaker;
-import JavaFX.Buttons;
 import javafx.stage.Stage;
+import java.io.IOException;
 
-public abstract class DaoPartVisitor extends DaoMaker implements Buttons {
+public abstract class DaoPartVisitor extends DaoMaker {
 
-    public abstract DaoPartVisitor visit(Stage stage, DaoPartVisitor daoPartVisitor);
+    public abstract DaoPartVisitor visit(Stage stage, DaoPartVisitor daoPartVisitor) throws IOException;
 
-    public abstract void create(Stage stage);
-
-    public abstract void read(Stage stage);
-
-    public abstract void update(Stage stage);
-
-    public abstract void delete(Stage stage);
-
-    public abstract Long inputNumber();
-
-    public abstract String inputString();
-
-    public abstract void welcome();
 }

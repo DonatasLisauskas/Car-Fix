6a30ec76e18e85e6078187fcc24a5f5aea0e20ca
donli <donliskas@gmail.com>
2020-09-09 19:29:59 +0300

    JavaFX/v1.3 Added fxml files and Controller.

--- src/main/java/CarFixJavaFX/Controllers/SelectDBController.java
+++ src/main/java/CarFixJavaFX/Controllers/SelectDBController.java
@@ -4,13 +4,8 @@ import Visitor_Pattern.CarDaoPartDisplayVisitor;
 import Visitor_Pattern.DaoPart;
 import Visitor_Pattern.Visitors.DaoVisitor;
 import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;

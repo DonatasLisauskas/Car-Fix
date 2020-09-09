28f70d9b008fc5a43f2d10ea7ee85d92e9434a06
donli <donliskas@gmail.com>
2020-09-09 19:50:56 +0300

    JavaFX/v1.3 Added fxml files and Controller.

--- src/main/java/Visitor_Pattern/Visitors/DaoVisitor.java
+++ src/main/java/Visitor_Pattern/Visitors/DaoVisitor.java
@@ -6,10 +6,12 @@ import Visitor_Pattern.DaoPartVisitor;
 import carfix.dao.CarDao;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class DaoVisitor implements DaoPart {
 
     @Override
-    public DaoPart accept(Stage stage, DaoPartVisitor daoPartVisitor) {
+    public DaoPart accept(Stage stage, DaoPartVisitor daoPartVisitor) throws IOException {
         daoPartVisitor.visit(stage,daoPartVisitor);
         return this;
     }

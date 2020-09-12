c2818c175ec035e0c1bf132f29375b98af9c834a
donli <donliskas@gmail.com>
2020-09-12 21:37:15 +0300

    Added Update operation with JavaFX. FX v.1.8.0

--- src/main/java/FXML_Controllers/Detail_FXML_Controllers/UpdateDetailController.java
+++ src/main/java/FXML_Controllers/Detail_FXML_Controllers/UpdateDetailController.java
@@ -1,16 +1,22 @@
 package FXML_Controllers.Detail_FXML_Controllers;
 
+import Facade_Pattern.DaoMaker;
 import carfix.dao.CarDao;
-import carfix.dao.DetailDao;
 import carfix.entities.Car;
 import carfix.entities.Detail;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
-public class UpdateDetailController {
+import java.io.IOException;
+
+public class UpdateDetailController extends DaoMaker {
 
     @FXML
-    private TextField id;
+    private TextField detailID;
 
     @FXML
     private TextField detailName;
@@ -19,44 +25,69 @@ public class UpdateDetailController {
     private TextField price;
 
     @FXML
-    private Car car;
+    private TextField carID;
 
     @FXML
-    private TextField carId;
-
-    @FXML
-    public void updateName() {
+    public void updateName() throws IOException {
         try {
-            Detail detail = new Detail();
-            DetailDao detailDao = new DetailDao();
-            detail = detailDao.getDetailById(Long.valueOf(id.getText()));
+            Detail detail = detailDao.getDetailById(Long.valueOf(detailID.getText()));
             detail.setDetailName(detailName.getText());
             detailDao.updateDetail(detail);
         } catch (RuntimeException e) {
         } // update is completed successful, but throw Runtime exception JavaFX.
+        finally {
+            try {
+                FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("/JavaFX/DatabaseUpdated.fxml"));
+                Parent root1 = (Parent) fxmlLoader1.load();
+                Stage stage1 = new Stage();
+                stage1.setScene(new Scene(root1));
+                stage1.show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     @FXML
-    public void updatePrice() {
+    public void updatePrice() throws IOException {
         try {
-            Detail detail = new Detail();
-            DetailDao detailDao = new DetailDao();
-            detail = detailDao.getDetailById(Long.valueOf(id.getText()));
+            Detail detail = detailDao.getDetailById(Long.valueOf(detailID.getText()));
             detail.setPrice(Long.valueOf(price.getText()));
             detailDao.updateDetail(detail);
         } catch (RuntimeException e) {
         } // update is completed successful, but throw Runtime exception JavaFX.
+        finally {
+            try {
+                FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("/JavaFX/DatabaseUpdated.fxml"));
+                Parent root1 = (Parent) fxmlLoader1.load();
+                Stage stage1 = new Stage();
+                stage1.setScene(new Scene(root1));
+                stage1.show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 
     @FXML
-    public void updateCarId() {
+    public void updateCarID() throws IOException {
         try {
-            Detail detail = new Detail();
-            DetailDao detailDao = new DetailDao();
-            detail = detailDao.getDetailById(Long.valueOf(id.getText()));
-            detail.setCar(car.getCarId());
+            Detail detail = detailDao.getDetailById(Long.valueOf(detailID.getText()));
+            Car car = carDao.getCarById(Long.valueOf(carID.getText()));
+            detail.setCar(car);
             detailDao.updateDetail(detail);
         } catch (RuntimeException e) {
         } // update is completed successful, but throw Runtime exception JavaFX.
+        finally {
+            try {
+                FXMLLoader fxmlLoader1 = new FXMLLoader(getClass().getResource("/JavaFX/DatabaseUpdated.fxml"));
+                Parent root1 = (Parent) fxmlLoader1.load();
+                Stage stage1 = new Stage();
+                stage1.setScene(new Scene(root1));
+                stage1.show();
+            } catch (IOException ex) {
+                ex.printStackTrace();
+            }
+        }
     }
 }

052b3545377ecf111503d89cb866c0668b4f8fad
donli <donliskas@gmail.com>
2020-09-13 13:30:59 +0300

    Added Failure Controllers/ JAVAFX.

--- src/main/java/FXML_Controllers/Failure_FXML_Controllers/CrudFailureController.java
+++ src/main/java/FXML_Controllers/Failure_FXML_Controllers/CrudFailureController.java
@@ -5,8 +5,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,11 +12,6 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 public class CrudFailureController extends Parent implements Initializable {
-
-    @FXML
-    private TextField textField;
-    @FXML
-    private Label label;
 
     @FXML
     private void createFailureButton() throws IOException {

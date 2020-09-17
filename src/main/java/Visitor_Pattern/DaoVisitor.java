19e7be8ffd0a3349da36e1af9858bbdcbab88209
donli <donliskas@gmail.com>
2020-09-17 22:13:51 +0300

    Added try - catch blocks.

--- src/main/java/Visitor_Pattern/DaoVisitor.java
+++ src/main/java/Visitor_Pattern/DaoVisitor.java
@@ -1,11 +1,20 @@
 package Visitor_Pattern;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.IOException;
 
 public class DaoVisitor implements DaoPart {
 
+    private static final Logger LOGGER = LogManager.getLogger(DaoVisitor.class);
+
     @Override
     public void accept(VisitPart visitPart, Visitors visitors) throws IOException {
-        visitPart.visit(visitors);
+        try {
+            visitPart.visit(visitors);
+        } catch (IOException ex) {
+            LOGGER.error(ex);
+        }
     }
 }

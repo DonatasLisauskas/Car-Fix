ae74b788c1e53b71aa53af207ddb1e5bf737d9b7
donli <donliskas@gmail.com>
2020-09-22 17:34:48 +0300

    The "DatabaseIsEmpty.fxml" file and The conditions of regexp is mounted on the DeleteCustomerController.java file. The exceptions are handled.

--- src/main/java/FXML_Controllers/Customer_FXML_Controllers/DeleteCustomerController.java
+++ src/main/java/FXML_Controllers/Customer_FXML_Controllers/DeleteCustomerController.java
@@ -25,9 +25,7 @@ public class DeleteCustomerController extends DaoMaker {
             Customer customer = customerDao.getCustomerById(Long.valueOf(number.getText()));
             String testID = number.getText();
 
-            if (!testID.matches(ID)) {
-                LoaderFXML.loadInvalidValueFXML();
-            } else if (testID.matches(ID) && customer != null) {
+            if (testID.matches(ID) && customer != null) {
                 customerDao.deleteCustomer(customer);
                 LOGGER.info("\u001B[33mDELETE Customer: Database is updated!\u001B[0m");
                 LoaderFXML.loadDatabaseUpdatedFXML();

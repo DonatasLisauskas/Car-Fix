0974d23e56d54b30d6c2f1b1ae46db25d8e7709b
donli <donliskas@gmail.com>
2020-09-22 19:10:18 +0300

    Annotations "@Pattern" of validations are connected with entities.

--- src/main/java/carfix/entities/Car.java
+++ src/main/java/carfix/entities/Car.java
@@ -6,8 +6,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Pattern;
 import java.util.List;
 
-import static carfix.Validation.Regexp.INVALID_SERIES_NAME;
-import static carfix.Validation.Regexp.SERIES_NAME;
+import static carfix.Validation.Regexp.*;
 
 @Entity
 @Getter
@@ -28,14 +27,17 @@ public class Car {
     @NonNull
     private String seriesName;
 
+    @Pattern(regexp = MANUFACTURE_YEAR,message = INVALID_MANUFACTURE_YEAR)
     @Column(name = "manufactureYear")
     @NonNull
     private Long manufactureYear;
 
+    @Pattern(regexp = ENGINE_DISPLACEMENT, message = INVALID_ENGINE_DISPLACEMENT)
     @Column(name = "engineDisplacement_L")
     @NonNull
     private String engineDisplacement_L;
 
+    @Pattern(regexp = ID_OR_NUMBER, message = INVALID_ID_OR_NUMBER)
     @Column(name = "enginePower_KW")
     @NonNull
     private Long enginePower_KW;
